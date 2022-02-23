4f75c1975cb0121681d25ff0ff31c45b404cadb0
magcipie2007 <magicpie2007@gmail.com>
2022-02-23 14:00:23 +0900

    Change the version of ui-react lib to downgrade

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-import { Authenticator } from '@aws-amplify/ui-react';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
 function App() {
   return (
-    <Authenticator>
-      {({ signOut, user }) => (
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-              <p>
-                Hey {user.username}, welcome to my channel, with auth!
-              </p>
-              <button onClick={signOut}>Sign out</button>
-          </header>
-        </div>
-      )}
-    </Authenticator>
+    <div className="App">
+      <header>
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>We now have Auth!</h1>
+      </header>
+      <AmplifySignOut />
+    </div>
   );
 }
 
-//export default App;
-export default App;
+export default withAuthenticator(App);
